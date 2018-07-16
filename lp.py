c1d04f7500a274ee0c5737beef9b54272e6969a8
Sam Estep <sam@samestep.com>
2018-07-16 13:50:53 -0400

    Generate triangulation

--- lp.py
+++ lp.py
@@ -1,11 +1,23 @@
+import math
 import matplotlib.pyplot as plt
 import networkx as nx
 import random as rand
+import scipy.spatial as space
+
+points = {i: (rand.random(), rand.random()) for i in range(10)}
+
+def weighted_edge(u, v):
+    x1, y1 = points[u]
+    x2, y2 = points[v]
+    return u, v, math.hypot(x2 - x1, y2 - y1)
+
+def triangle_edges(triangle):
+    indices = [(0, 1), (1, 2), (2, 0)]
+    return (weighted_edge(triangle[i], triangle[j]) for i, j in indices)
 
 G = nx.Graph()
-nodes = ((i, {'pos': (rand.random(), rand.random())}) for i in range(10))
-G.add_nodes_from(nodes)
-edges = rand.sample([(u, v) for u in G.nodes for v in G.nodes if u < v], 20)
-G.add_edges_from(edges)
-nx.draw(G, pos={v: data['pos'] for v, data in G.nodes(data=True)})
+G.add_nodes_from((v, {'pos': p}) for v, p in points.items())
+for triangle in space.Delaunay(list(points.values())).simplices:
+    G.add_weighted_edges_from(triangle_edges(triangle))
+nx.draw_networkx(G, pos=points)
 plt.show()
