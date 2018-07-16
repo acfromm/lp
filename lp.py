953a0f776b1730e9e6a38f625d91f18ce7bf20ad
Sam Estep <sam@samestep.com>
2018-07-16 15:09:32 -0400

    Print tree weights and sums of path lengths

--- lp.py
+++ lp.py
@@ -15,9 +15,12 @@ def triangle_edges(triangle):
     indices = [(0, 1), (1, 2), (2, 0)]
     return (weighted_edge(triangle[i], triangle[j]) for i, j in indices)
 
-def shortest_path_tree_edges(G, source):
+def shortest_path_tree(G, source):
     predecessors, _ = nx.dijkstra_predecessor_and_distance(G, source)
-    return ((u, v) for u, parents in predecessors.items() for v in parents)
+    edges = (weighted_edge(u, v) for u, vs in predecessors.items() for v in vs)
+    T = nx.Graph()
+    T.add_weighted_edges_from(edges)
+    return T
 
 def color(x, s1, s2):
     if x in s1:
@@ -30,9 +33,15 @@ G.add_nodes_from((v, {'pos': p}) for v, p in points.items())
 for triangle in space.Delaunay(list(points.values())).simplices:
     G.add_weighted_edges_from(triangle_edges(triangle))
 edgelist = list(G.edges)
-mst = set(map(frozenset, nx.minimum_spanning_tree(G).edges))
-spt = set(map(frozenset, shortest_path_tree_edges(G, 0)))
-edge_color = [color(frozenset(edge), mst, spt) for edge in edgelist]
+mst = nx.minimum_spanning_tree(G)
+spt = shortest_path_tree(G, 0)
+mst_edges, spt_edges = (set(map(frozenset, T.edges)) for T in [mst, spt])
+print('MST weight:', mst.size(weight='weight'))
+print('SPT weight:', spt.size(weight='weight'))
+print()
+print('MST paths:', sum(nx.single_source_dijkstra_path_length(mst, 0).values()))
+print('SPT paths:', sum(nx.single_source_dijkstra_path_length(spt, 0).values()))
+edge_color = [color(frozenset(edge), mst_edges, spt_edges) for edge in edgelist]
 nx.draw_networkx(
     G, pos=points, edgelist=edgelist,
     node_color='white', edge_color=edge_color
