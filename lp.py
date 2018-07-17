a58e741d1b7586d2be59caf58dbdfa7fe3b64270
Sam Estep <sam@samestep.com>
2018-07-17 11:24:25 -0400

    Add LP SPT algorithm

--- lp.py
+++ lp.py
@@ -1,3 +1,4 @@
+import gurobipy as gurobi
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -22,6 +23,28 @@ def shortest_path_tree(G, source):
     T.add_weighted_edges_from(edges)
     return T
 
+def sum_paths(G, source):
+    return sum(nx.single_source_dijkstra_path_length(G, source).values())
+
+def lp_spt(G, source):
+    H = G.to_directed()
+    m = gurobi.Model()
+    m.setParam('OutputFlag', False)
+    x = {}
+    for i, j in H.edges:
+        x[i, j] = m.addVar(name='x[%s,%s]' % (i, j))
+    m.setObjective(sum(H.edges[i, j]['weight'] * x[i, j] for i, j in H.edges))
+    for i in H.nodes:
+        inflow = sum(x[j, i] for j, _ in H.in_edges(i))
+        outflow = sum(x[i, j] for _, j in H.out_edges(i))
+        rhs = 1 - len(H) if i == source else 1
+        m.addConstr(inflow - outflow == rhs, str(i))
+    m.optimize()
+    edges = (weighted_edge(u, v) for (u, v), var in x.items() if var.x > 0)
+    T = nx.Graph()
+    T.add_weighted_edges_from(edges)
+    return T
+
 def color(x, s1, s2):
     if x in s1:
         return 'purple' if x in s2 else 'red'
@@ -39,8 +62,14 @@ spt = shortest_path_tree(G, 0)
 print('MST weight:', mst.size(weight='weight'))
 print('SPT weight:', spt.size(weight='weight'))
 print()
-print('MST paths:', sum(nx.single_source_dijkstra_path_length(mst, 0).values()))
-print('SPT paths:', sum(nx.single_source_dijkstra_path_length(spt, 0).values()))
+print('MST paths: ', sum_paths(mst, 0))
+print('SPT paths: ', sum_paths(spt, 0))
+
+print()
+alt_spt = lp_spt(G, 0)
+
+print('LP SPT weight:', alt_spt.size(weight='weight'))
+print('LP SPT paths: ', sum_paths(alt_spt, 0))
 
 edgelist = list(G.edges)
 mst_edges, spt_edges = (set(map(frozenset, T.edges)) for T in [mst, spt])
